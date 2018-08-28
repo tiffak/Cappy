0992f3ca2a5923c436a72b7bb4d4f597780dae9f
Tiffany Kuo <tiffa.kuo@mail.utoronto.ca>
2018-08-28 17:24:32 -0400

    Added DatePicker, TimePicker and Day Spinner to AddNewActivity

--- app/src/main/java/com/example/cappy/AddNewActivity.java
+++ app/src/main/java/com/example/cappy/AddNewActivity.java
@@ -1,9 +1,14 @@
 package com.example.cappy;
 
+import android.app.TimePickerDialog;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class AddNewActivity extends AppCompatActivity {
 
@@ -17,5 +22,17 @@ public class AddNewActivity extends AppCompatActivity {
         ActionBar ab = getSupportActionBar();
         ab.setTitle("New Course");
         ab.setDisplayHomeAsUpEnabled(true);
+//        createSpinner();
     }
+
+//    private void createSpinner(){
+//        Spinner spinner = (Spinner) findViewById(R.id.weekSpinner);
+//        // Create an ArrayAdapter using the string array and a default spinner layout
+//        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+//                R.array.week_array, android.R.layout.simple_spinner_item);
+//        // Specify the layout to use when the list of choices appears
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        // Apply the adapter to the spinner
+//        spinner.setAdapter(adapter);
+//    }
 }
